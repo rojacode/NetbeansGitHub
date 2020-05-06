54f0a4ffd87599c756bf2a0c94a05847b4440a7e
Robinson Jara <robi_lf@yahoo.com>
2020-05-06 20:11:54 +0200

    Añadido rectángulo

---
+++ src/circulo/Rectangulo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circulo;
+
+/**
+ *
+ * @author robi_
+ */
+public class Rectangulo {
+    float alto, ancho;
+    public Rectangulo(float l1, float l2){
+        alto=l1;
+        ancho=l2;
+        
+    }
+    
+}
